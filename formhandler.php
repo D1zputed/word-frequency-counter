fb308ba6bfe5f10609af5d4394aa1a7f4ffdefa2
D1zputed <asaphdelcoro987@gmail.com>
2023-09-23 15:25:40 +0800

    added functionality to the formahandler

--- formhandler.php
+++ formhandler.php
@@ -4,5 +4,32 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
     $text = htmlspecialchars($_POST["inputedText"]);
     $sortBy = htmlspecialchars($_POST["sort"]);
     $wordLimit = htmlspecialchars($_POST["wordLimit"]);
+    $allWords = preg_split("/[\s,.]+/", $text);
+    $lowerCase = array_map('strtolower', $allWords);
+    $countWords = array_count_values($allWords);
+    $commonWords = array("and", "And", "Or", "or", "the", "The", "in", "In");
+    if($sortBy == "asc"){
+        asort($countWords);
+    }
+    else{
+        arsort($countWords);
+    }
+    $count = 0;
+    foreach($countWords as $word => $occurence){
+        if(in_array($word, $commonWords) == false){
+            echo "<strong>$word</strong> mentioned $occurence time/s";
+            $count += 1;
+        }
+        echo "<br>";
+        if($count == $wordLimit){
+            break;
+        }
+    }
+    
+
 }
-echo $wordLimit + 1;
+else{
+    header("Location: index.php");
+}
+
+
